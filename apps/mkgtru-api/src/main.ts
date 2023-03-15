70e0a5bbe10e84b398068fd932f1b50a556cefb0
AlexC-ux <eunv2004@gmail.com>
2023-03-15 11:32:22 +0300

    port upd

--- apps/mkgtru-api/src/main.ts
+++ apps/mkgtru-api/src/main.ts
@@ -3,6 +3,6 @@ import { MkgtruApiModule } from './mkgtru-api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MkgtruApiModule);
-  await app.listen(process.env.port||3000);
+  await app.listen(8080);
 }
 bootstrap();
