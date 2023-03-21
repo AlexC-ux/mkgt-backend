d41ddd640019bbb962e828c5e3b5505d9e89133a
AlexC-ux <eunv2004@gmail.com>
2023-03-21 19:11:35 +0300

    123

--- apps/mkgtru-api/src/mkgtru-api.controller.ts
+++ apps/mkgtru-api/src/mkgtru-api.controller.ts
@@ -31,7 +31,6 @@ const tokenSchema = {
  * @typedef {MkgtruApiController}
  */
 @Controller("mkgtru-api")
-@UseInterceptors(CacheInterceptor)
 @ApiTags('mkgtru-api')
 export class MkgtruApiController {
   constructor(private readonly mkgtruApiService: MkgtruApiService) { }
@@ -50,6 +49,8 @@ export class MkgtruApiController {
   @ApiResponse({ status: HttpStatus.OK, description: "Success" })
   @ApiResponse({ status: HttpStatus.FORBIDDEN, description: "Wrong api key" })
   @Get("changes")
+  @UseInterceptors(CacheInterceptor)
+  @CacheTTL(5)
   @UseGuards(RequireApiKeyGuard)
   async getChanges(@Query("territory") territory: territories): Promise<ITitledDocumentInfo> {
     return this.mkgtruApiService.getChanges(territory);
