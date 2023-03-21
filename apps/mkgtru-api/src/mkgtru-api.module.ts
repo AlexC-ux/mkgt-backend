5969b8c369f5368a0dbb49fed3d29d3860cb8bdc
AlexC-ux <eunv2004@gmail.com>
2023-03-21 18:53:41 +0300

    cache upd

--- apps/mkgtru-api/src/mkgtru-api.module.ts
+++ apps/mkgtru-api/src/mkgtru-api.module.ts
@@ -1,13 +1,19 @@
-import { Module, CacheModule } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { MkgtruApiController } from './mkgtru-api.controller';
 import { MkgtruApiService } from './mkgtru-api.service';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: "./apps/mkgtru-api/.env" }),
     CacheModule.register(),
   ],
   controllers: [MkgtruApiController],
-  providers: [MkgtruApiService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+    MkgtruApiService],
 })
 export class MkgtruApiModule { }
