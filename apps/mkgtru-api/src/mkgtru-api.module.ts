5c65c8e877d708a42c26819783ad161e1055db72
AlexC-ux <eunv2004@gmail.com>
2023-03-21 19:10:55 +0300

    123

--- apps/mkgtru-api/src/mkgtru-api.module.ts
+++ apps/mkgtru-api/src/mkgtru-api.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: "./apps/mkgtru-api/.env" }),
-    CacheModule.register({ isGlobal: true, ttl: 5, max: 20 }),
+    CacheModule.register({ isGlobal: true, ttl: 5, max: 30 }),
   ],
   controllers: [MkgtruApiController],
   providers: [MkgtruApiService],
