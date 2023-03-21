c8832d3b7a9a123c725432966e4b73464f371761
AlexC-ux <eunv2004@gmail.com>
2023-03-21 19:44:31 +0300

    123

--- apps/mkgtru-api/src/mkgtru-api.controller.ts
+++ apps/mkgtru-api/src/mkgtru-api.controller.ts
@@ -51,6 +51,7 @@ export class MkgtruApiController {
   @ApiResponse({ status: HttpStatus.OK, description: "Success" })
   @ApiResponse({ status: HttpStatus.FORBIDDEN, description: "Wrong api key" })
   @UseGuards(RequireApiKeyGuard)
+  @CacheTTL(5000)
   @Get("changes")
   async getChanges(@Query("territory") territory: territories): Promise<ITitledDocumentInfo> {
     return this.mkgtruApiService.getChanges(territory);
