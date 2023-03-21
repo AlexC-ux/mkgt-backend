9e326fa7c498d2a368e028ccdd180c824fe8c0fa
AlexC-ux <eunv2004@gmail.com>
2023-03-21 19:26:49 +0300

    123

--- apps/mkgtru-api/src/mkgtru-api.module.ts
+++ apps/mkgtru-api/src/mkgtru-api.module.ts
@@ -6,7 +6,7 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: "./apps/mkgtru-api/.env" }),
-    CacheModule.register(),
+    CacheModule.register({ttl:10000, max:30}),
   ],
   controllers: [MkgtruApiController],
   providers: [MkgtruApiService],
