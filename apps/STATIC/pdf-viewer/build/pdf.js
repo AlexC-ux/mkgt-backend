95f30ac003504e3a0379986dd5da36edc15a50a2
AlexC-ux <eunv2004@gmail.com>
2023-03-21 18:43:27 +0300

    pdf

--- apps/STATIC/pdf-viewer/build/pdf.js
+++ apps/STATIC/pdf-viewer/build/pdf.js
@@ -13244,7 +13244,7 @@ class PDFFetchStreamReader {
     this._isRangeSupported = !source.disableRange;
     this._headers = createHeaders(this._stream.httpHeaders);
     const url = source.url;
-    fetch('http://45.87.247.20:6036/'+url, createFetchOptions(this._headers, this._withCredentials, this._abortController)).then(response => {
+    fetch('https://api.codetabs.com/v1/proxy/?quest='+url, createFetchOptions(this._headers, this._withCredentials, this._abortController)).then(response => {
       if (!(0, _network_utils.validateResponseStatus)(response.status)) {
         throw (0, _network_utils.createResponseStatusError)(response.status, url);
       }
