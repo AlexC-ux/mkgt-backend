956bcab1fb3ccdb98b024983e2def858a7a5276a
AlexC-ux <eunv2004@gmail.com>
2023-11-07 03:22:53 +0300

    static added

--- apps/mkgtru-api/src/mkgtru-api.module.ts
+++ apps/mkgtru-api/src/mkgtru-api.module.ts
@@ -2,10 +2,14 @@ import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { MkgtruApiController } from './mkgtru-api.controller';
 import { MkgtruApiService } from './mkgtru-api.service';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: "./apps/mkgtru-api/.env" }),
     CacheModule.register({ max:100}),
+    ServeStaticModule.forRoot({
+      rootPath:"./apps/static"
+    })
   ],
   controllers: [MkgtruApiController],
   providers: [MkgtruApiService],
