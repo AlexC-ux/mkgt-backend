f128c51c3cad5fc64cf2a072a87c2db6ec7a5af7
AlexC-ux <eunv2004@gmail.com>
2023-11-07 04:05:09 +0300

    params.query logs

--- apps/mkgtru-api/src/logger/index.ts
+++ apps/mkgtru-api/src/logger/index.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, query } from 'express';
 export function LogRequest(req: Request, res: Response, next: NextFunction) {
-    console.info(buildLogString({path:req.url, method:req.method, ip:req.ip, httpV:req.httpVersion, token:`${req.headers.authorization}`}))
+    console.info(buildLogString({path:req.url, method:req.method, ip:req.ip, httpV:req.httpVersion, token:`${req.headers.authorization}`, query:req.query}))
     next();
 }
 
-function buildLogString(params: { path: string, method:string, ip:string, httpV:string, token:string }): string {
-    return `${new Date().toLocaleString("ru")} ${params.ip} ${params.method} ${params.httpV} ${params.path} ${params.token}`
+function buildLogString(params: { path: string, method:string, ip:string, httpV:string, token:string, query:Object }): string {
+    return `${new Date().toLocaleString("ru")} ${params.ip} ${params.method} ${params.httpV} ${params.path} ${JSON.stringify(params.query)} ${params.token}`
 }
